efe7b80f2af5918b6f2896b2587096e15e6f6607
ninad259 <ninadvij@buffalo.edu>
2014-11-22 01:32:38 -0500

    changes to tune parameters for the algorithm

--- src/mcl/MCLDriver.java
+++ src/mcl/MCLDriver.java
@@ -3,17 +3,27 @@ package mcl;
 public class MCLDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		MCLAlgorithm mcl = null;
-		mcl = new MCLAlgorithm("attweb_net.txt");
-//		mcl= new MCLAlgorithm("physics_collaboration_net.txt");
+		
+		// power is constant = 2
+		
+		double r = 0.0;
+		
+		// r = 1.36;
+//		mcl = new MCLAlgorithm("attweb_net.txt");
+		
+		 r = 1.21;
+//		 r = 1.25;
+//		 r = 1.26;
+		mcl= new MCLAlgorithm("physics_collaboration_net.txt");
+		
+		// r = 1.19;
 //		mcl = new MCLAlgorithm("yeast_undirected_metabolic.txt");
 		
-		for(int r=2; r<5; r++){
+//		for(int i=0; i<10;i++){
+//			r = 1.15+0.01*i;
 			System.out.println("r: "+r);
-			mcl.clustering(r);
-		}
-			
+			mcl.clustering(r);	
+//		}
 	}
 }
