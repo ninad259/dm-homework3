75789b594515e415ec0d4d0fcf33f734b92a94e3
ninad259 <ninadvij@buffalo.edu>
2014-11-22 01:31:41 -0500

    added file writing functionality

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -4,9 +4,12 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.Scanner;
 
@@ -38,5 +41,19 @@ public class FileUtils {
 		}else{
 			return null;
 		}
+	}
+
+	public BufferedWriter writeInFile(String fileName){
+		File file = new File(resourceFolder+"/"+fileName);
+		BufferedWriter writer = null;
+		try {
+			if(!file.exists()){
+				file.createNewFile();
+			}
+			writer = new BufferedWriter(new FileWriter(file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return writer;
 	}
 }
